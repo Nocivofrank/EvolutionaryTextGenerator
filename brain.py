c6f611a0d4c0f4a48bec25700e7993201590e18d
Nocivofrank <sosatony111@gmail.com>
2026-01-08 15:18:43 -0600

    Updated the mutation process

--- brain.py
+++ brain.py
@@ -5,46 +5,86 @@ class Brain():
 
     #brian row and columns
     input_size = 100
+
     hidden_size = 20000
-    hidden2_size = 20000
-    hidden3_size = 20000
+    hidden2_size = 4000
+    hidden3_size = 4000
+    hidden4_size = 2000
+    hidden5_size = 400
+    hidden6_size = 200
     output_size = 97
+
     
     def __init__(self, id):
         # weights and biases
         #for input size
-        self.W1 = np.array([[Brain.random_range(-1, 1) for _ in range(Brain.input_size)]
-            for _ in range(Brain.hidden_size)])
+        self.W1 = np.array([[Brain.random_range(-1, 1) for _ in range(Brain.input_size)] for _ in range(Brain.hidden_size)])
+        self.W1_prev = self.W1
 
         self.b1 = np.array([Brain.random_range(-1, 1) for _ in range(Brain.hidden_size)])
+        self.b1_prev = self.b1
 
         #for hidden 1
-        self.W2 = np.array([[Brain.random_range(-1, 1) for _ in range(Brain.hidden_size)] 
-            for _ in range(Brain.hidden2_size)])
+        self.W2 = np.array([[Brain.random_range(-1, 1) for _ in range(Brain.hidden_size)] for _ in range(Brain.hidden2_size)])
+        self.W2_prev = self.W2
 
         self.b2 = np.array([Brain.random_range(-1, 1) for _ in range(Brain.hidden2_size)])
+        self.b2_prev = self.b2
 
         #for hidden 2
-        self.W3 = np.array([[Brain.random_range(-1, 1) for _ in range(Brain.hidden2_size)]
-            for _ in range(Brain.hidden3_size)])
+        self.W3 = np.array([[Brain.random_range(-1, 1) for _ in range(Brain.hidden2_size)] for _ in range(Brain.hidden3_size)])
+        self.W3_prev = self.W3
 
         self.b3 = np.array([Brain.random_range(-1, 1) for _ in range(Brain.hidden3_size)])
+        self.b3_prev = self.b3
+
+        #for hidden 3
+        self.W4 = np.array([[Brain.random_range(-1, 1) for _ in range(Brain.hidden3_size)] for _ in range(Brain.hidden4_size)])
+        self.W4_prev = self.W4
+
+        self.b4 = np.array([Brain.random_range(-1, 1) for _ in range(Brain.hidden4_size)])
+        self.b4_prev = self.b4
+
+        #for hidden 4
+        self.W5 = np.array([[Brain.random_range(-1, 1) for _ in range(Brain.hidden4_size)] for _ in range(Brain.hidden5_size)])
+        self.W5_prev = self.W5
+
+        self.b5 = np.array([Brain.random_range(-1, 1) for _ in range(Brain.hidden5_size)])
+        self.b5_prev = self.b5
+
+        #for hidden 5
+        self.W6 = np.array([[Brain.random_range(-1, 1) for _ in range(Brain.hidden5_size)] for _ in range(Brain.hidden6_size)])
+        self.W6_prev = self.W6
+
+        self.b6 = np.array([Brain.random_range(-1, 1) for _ in range(Brain.hidden6_size)])
+        self.b6_prev = self.b6
 
         #for output
-        self.W4 = np.array([[Brain.random_range(-1, 1) for _ in range(Brain.hidden3_size)]
-            for _ in range(Brain.output_size)])
+        self.W7 = np.array([[Brain.random_range(-1, 1) for _ in range(Brain.hidden6_size)] for _ in range(Brain.output_size)])
+        self.W7_prev = self.W7
 
-        self.b4 = np.array([Brain.random_range(-1, 1) for _ in range(Brain.output_size)])
+        self.b7 = np.array([Brain.random_range(-1, 1) for _ in range(Brain.output_size)])
+        self.b7_prev = self.b7
 
         self.information = np.zeros(Brain.input_size)
         self.id = id
+        self.prev_token = 0
 
     def sigmoid(x):
-        return np.where(
-            x >= 0,
-            1 / (1 + np.exp(-x)),
-            np.exp(x) / (1 + np.exp(x))
-        )
+        out = np.empty_like(x, dtype=float)
+
+        pos_mask = x >= 0
+        neg_mask = ~pos_mask
+
+        # safe for positive x
+        out[pos_mask] = 1 / (1 + np.exp(-x[pos_mask]))
+
+        # safe for negative x
+        ex = np.exp(x[neg_mask])
+        out[neg_mask] = ex / (1 + ex)
+
+        return out
+
 
     def brainThink(self):
         # Layer 1
@@ -59,17 +99,45 @@ class Brain():
         z3 = np.dot(self.W3, h2) + self.b3
         h3 = Brain.sigmoid(z3)
 
-        # Output layer
+        # Layer 4
         z4 = np.dot(self.W4, h3) + self.b4
-        out = Brain.sigmoid(z4)
+        h4 = Brain.sigmoid(z4)
+
+        # Layer 5
+        z5 = np.dot(self.W5, h4) + self.b5
+        h5 = Brain.sigmoid(z5)
+
+        # Layer 6
+        z6 = np.dot(self.W6, h5) + self.b6
+        h6 = Brain.sigmoid(z6)
+
+        #output layer
+        z7 = np.dot(self.W7, h6) + self.b7
+        out = Brain.sigmoid(z7)
 
         # z5 = np.dot(self.W4, h3) + self.b4
         # action = Brain.sigmoid(z5)
 
         return out#mn , action
 
-    def brainMutate(self, chance=.5, super_chance=0.5, strength=.5, super_strength=-1.0):
-        layers = [self.W1, self.b1, self.W2, self.b2, self.W3, self.b3, self.W4, self.b4]
+    def brainMutate(self, chance=0.5, super_chance=0.1, strength=0.5, super_strength=0.7):
+        print(f"Brain {self.id} mutating")
+        layers = [self.W1, self.b1, self.W2, self.b2, self.W3, self.b3, self.W4, self.b4, self.W5, self.b5, self.W6, self.b6, self.W7, self.b7]
+
+        self.W1_prev = self.W1
+        self.b1_prev = self.b1
+        self.W2_prev = self.W2
+        self.b2_prev = self.b2
+        self.W3_prev = self.W3
+        self.b3_prev = self.b3
+        self.W4_prev = self.W4
+        self.b4_prev = self.b4
+        self.W5_prev = self.W5
+        self.b5_prev = self.b5
+        self.W6_prev = self.W6
+        self.b6_prev = self.b6
+        self.W7_prev = self.W7
+        self.b7_prev = self.b7
 
         for layer in layers:
             # Normal mutation mask
@@ -85,6 +153,22 @@ class Brain():
             # Super mutation overrides normal one
             mutation_super = np.random.uniform(-super_strength, super_strength, layer.shape)
             layer += super_mask * mutation_super
+
+    def revertToPreviousBrain(self):
+        self.W1 = self.W1_prev
+        self.b1 = self.b1_prev
+        self.W2 = self.W2_prev
+        self.b2 = self.b2_prev
+        self.W3 = self.W3_prev
+        self.b3 = self.b3_prev
+        self.W4 = self.W4_prev
+        self.b4 = self.b4_prev
+        self.W5 = self.W5_prev
+        self.b5 = self.b5_prev
+        self.W6 = self.W6_prev
+        self.b6 = self.b6_prev
+        self.W7 = self.W7_prev
+        self.b7 = self.b7_prev
 
     def random_range(a, b):
         return a + (b - a) * (secrets.randbits(52) / (1 << 52))
--- main.py
+++ main.py
@@ -2,13 +2,11 @@ import brain as br
 
 brains = []
 
-amount_of_brains = 10
+amount_of_brains = 5
 
 for i in range(amount_of_brains):
     brains.append(br.Brain(i))
     print(brains[i], brains[i].id)
-
-context_lenght = 10
 
 text = "Hello there world how are you today and how are we feeling?"
 text_new = ""
@@ -26,22 +24,22 @@ ascii_dict = {
 
 # print(repr(ascii_dict[29]))
 
-for i in range(context_lenght):
-    text_new += text[i]
+# for i in range(context_lenght):
+#     text_new += text[i]
 
 while True:
     print("\n")
     user_input = input("User: ")
-    refined_input = ""
-    for i in range(context_lenght):
+    raw_input = ""
+    for i in range(br.Brain.input_size):
         if i < len(user_input):
-            refined_input += user_input[i]
+            raw_input += user_input[i]    
 
     for i in brains:
-        for j in range(context_lenght):
+        for j in range(br.Brain.input_size):
             # print(j)
-            if j < len(refined_input):
-                i.information[j] = ord(refined_input[j])
+            if j < len(raw_input):
+                i.information[j] = ord(raw_input[j])
             else:
                 i.information[j] = 0
 
@@ -59,4 +57,60 @@ while True:
             count += 1
             # print(i.brainThink())
         biggest += 29
-        print(f"{refined_input}{ascii_dict[biggest]}")
+        i.prev_token = biggest
+        print(f"{raw_input}{ascii_dict[biggest]}, prev token: {i.prev_token}")
+
+    expected_output = input("Expected Output: ")
+
+    found_match = False
+    prev_mean = 0
+    mean = 0
+    closer = False
+    mult = 1
+
+    while not found_match:
+        mean = 0
+        for i in brains:
+            mean += i.prev_token
+            if ascii_dict[i.prev_token] == expected_output:
+                found_match = True
+                for j in brains:
+                    if i.id != j.id:
+                        j.W1 = i.W1
+                        j.b1 = i.b1
+                        j.W2 = i.W2
+                        j.b2 = i.b2
+                        j.W3 = i.W3
+                        j.b3 = i.b3
+                        j.W4 = i.W4
+                        j.b4 = i.b4
+                        j.W5 = i.W5
+                        j.b5 = i.b5
+                        j.W6 = i.W6
+                        j.b6 = i.b6
+                        j.W7 = i.W7
+                        j.b7 = i.b7
+                        j.brainMutate()
+                break
+            else:
+                i.revertToPreviousBrain()
+                mean = mean / amount_of_brains
+
+                i.brainMutate()
+                
+                biggest = 0
+                thoughts = i.brainThink()
+                
+                count = 0
+                for j in thoughts:
+                    if j > thoughts[biggest]:
+                        biggest = count
+                    count += 1
+                biggest += 29
+                i.prev_token = biggest
+                print(f"{raw_input}{ascii_dict[biggest]}, prev token: {i.prev_token}")
+
+        print(f"Mean: {mean}, Expected Value: {ord(expected_output)}")
+    
+
+
